c4be066ee5cf4fbe27924df921b2ed6b2ed88762
huseyincaliskan <14217365+sakirma@users.noreply.github.com>
2024-09-26 18:16:22 +0300

    quick testing with cilium

--- cilium/cilium.sh
+++ cilium/cilium.sh
@@ -5,9 +5,20 @@ helm repo add cilium https://helm.cilium.io/
 docker pull quay.io/cilium/cilium:v1.16.1
 kind load docker-image quay.io/cilium/cilium:v1.16.1
 
-helm upgrade --install cilium cilium/cilium --version 1.16.1 \
-    --namespace kube-system \
-    --set image.pullPolicy=IfNotPresent \
-    --set kubeProxyReplacement=true \
-    --set k8sServiceHost=kind-control-plane \
-    --set k8sServicePort=6443
+cilium install --version 1.16.1
+
+# This costs extra resource to have lower latency and higher through output
+#helm install cilium cilium/cilium --version 1.16.1 -n kube-system \
+#    --set routingMode=native \
+#    --set bpf.datapathMode=netkit \
+#    --set bpf.masquerade=true \
+#    --set ipv4.enabled=true \
+#    --set enableIPv4BIGTCP=true \
+#    --set ipam.mode=kubernetes \
+#    --set autoDirectNodeRoutes=true \
+#    --set ipv4NativeRoutingCIDR="10.0.0.0/8" \
+#    --set kubeProxyReplacement=true \
+#    --set bandwidthManager.enabled=true \
+#    --set bandwidthManager.bbr=true \
+#    --set k8sServiceHost=kind-control-plane \
+#    --set k8sServicePort=6443
--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 # [x] cilium
 # [-] -istio- *won't install it since EBPF and Cilium can outperform istio.
-# [ ] I want to connect some services with each other
-#   [ ] research what cilium service mesh offers.
+# [x] I want to connect some services with each other
+#   [x] research what cilium service mesh offers.
 # [ ] connect two services with go code using TCP connection
 #   [ ] connect with a different service if the current one is dead.
 # [ ] Check out about Kafka.
